8d9c7f89450e24502e3594086a768e00eceeece6
fvillalobos <401666+manteca@users.noreply.github.com>
2018-06-23 16:57:12 +0200

    Add option to print result an converted from unicode fisrt. Just for testing

--- save_tracks.py
+++ save_tracks.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3.5
+
 import sys
 import spotipy
 import spotipy.util as util
+
+import collections
+
 import os
 import math
 import json
@@ -17,14 +22,29 @@ else:
     print("Usage: %s username" % (sys.argv[0],))
     sys.exit()
 
-# Promte for user permission
+
+def convert(data):
+    if isinstance(data, basestring):
+        return str(data)
+    elif isinstance(data, collections.Mapping):
+        return dict(map(convert, data.iteritems()))
+    elif isinstance(data, collections.Iterable):
+        return type(data)(map(convert, data))
+    else:
+        return data
+
+# Promt for user permission
 token = util.prompt_for_user_token(username, scope)
 
 if token:
     cantidad_x_pag = 50
     cantidad_pag = 0
     sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks(1)
+    results = sp.current_user_saved_tracks(1,1)
+
+    # Save data in jason format. Transform Unicode to string FOR TEST
+    # print(convert(results))
+
     total =  results["total"]
 
     cantidad_pag = int(math.ceil(total / (cantidad_x_pag *1.0)))
@@ -35,31 +55,23 @@ if token:
 
     # TODO: Se repite el ultimo y el primer registro etre cada iteracion
     merged_dict = []
-    for x in range(0, cantidad_pag):
-        results = sp.current_user_saved_tracks(cantidad_x_pag, x)
-        data = results['items']
-        i=0
-        while i < len(data):
-            merged_dict.append(data[i])
-            i += 1
 
-        # if merged_dict != '':
-        #     # merged_dict = {key: value for (key, value) in (results['items'].items() + merged_dict.items())}
-        #     # merged_dict = merged_dict + ',' + json.dumps(results['items'])
-        #     merged_dict.concat(results['items'])
-        #     print "XXXXXPASESSSEDXXXX"
-        #
-        #     [{i:j for x in t for i,j in x.items()}]
-        # else:
-        #     merged_dict = results['items']
 
-        for item in results['items']:
-            track = item['track']
-            print(track['name'].encode('utf-8') + ' - ' + track['artists'][0]['name'].encode('utf-8'))
-            f.write(track['name'].encode('utf-8') + ' - ' + track['artists'][0]['name'].encode('utf-8') + '\n')
-
-    fjson.write(json.dumps(merged_dict))
-    fjson.close()
+    # for x in range(0, cantidad_pag):
+    #     results = sp.current_user_saved_tracks(cantidad_x_pag, x)
+    #     data = results['items']
+    #     i=0
+    #     while i < len(data):
+    #         merged_dict.append(data[i])
+    #         i += 1
+    #
+    #     for item in results['items']:
+    #         track = item['track']
+    #         print(track['name'].encode('utf-8') + ' - ' + track['artists'][0]['name'].encode('utf-8'))
+    #         f.write(track['name'].encode('utf-8') + ' - ' + track['artists'][0]['name'].encode('utf-8') + '\n')
+    #
+    # fjson.write(json.dumps(merged_dict))
+    # fjson.close()
     # f.close()
 else:
     print("Can't get token for", username)
